b7a7b2fe6cfcb3d9106ddadff4b96462b3c6ae48
miticn <101733519+miticn@users.noreply.github.com>
2022-06-24 10:03:58 -0700

    Clean

--- build/lib/mem.d
+++
@@ -1,5 +0,0 @@
-build/lib/mem.o: lib/mem.c lib/mem.h lib/hw.h
-
-lib/mem.h:
-
-lib/hw.h:
--- build/src/main.d
+++
@@ -1,8 +0,0 @@
-build/src/main.o: src/main.c src/../lib/console.h src/../lib/mem.h \
- src/../lib/hw.h
-
-src/../lib/console.h:
-
-src/../lib/mem.h:
-
-src/../lib/hw.h:
